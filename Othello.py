43ee6c3e007ca0b7ea99af207938464bc30d7833
ProAltro <90308389+ProAltro@users.noreply.github.com>
2021-10-27 15:20:50 +0530

    move() function
    Added move function (yet to be tested)

--- Othello.py
+++ Othello.py
@@ -6,8 +6,8 @@ dlcorner = '└'
 dplus = '┬'
 lplus = '┤'
 vedge = '│'
-rplus = '├'
 plus =  '┼'
+rplus = '├'
 uplus = '┴'
 
 pieces = {-1:"O",0:" ",1:"X"}
@@ -51,11 +51,96 @@ board[3][3],board[4][4] = -1,-1
 board[3][4],board[4][3] = 1,1
 
 
-def isValid(p,a):
+def move(p,a):
     px,py = p
-    for i in range(8):
-        if board[px][i] == a:
-            pass
-    
+    flag = False
+
+    #Right
+    for i in range(px+1,8):
+        if i == px+1 and board[i][py] != -a:
+            break
+        elif board[i][py] == 0:
+            break
+        elif board[i][py] == a:
+            flag = True
+            for j in range(px+1,i):
+                board[j][py] = a
+
+    #Left        
+    for i in range(px-1,-1,-1):
+        if i == px-1 and board[i][py] != -a:
+            break
+        elif board[i][py] == 0:
+            break
+        elif board[i][py] == a:
+            flag = True
+            for j in range(i,px):
+                board[j][py] = a
+    #Top            
+    for i in range(py+1,8):
+        if i == py+1 and board[px][i] != -a:
+            break
+        elif board[px][i] == 0:
+            break
+        elif board[px][i] == a:
+            flag = True
+            for j in range(py+1,i):
+                board[px][j] = a
+            
+        
+    #Bottom   
+    for i in range(py-1,-1,-1):
+        if i == py-1 and board[px][i] != -a:
+            break
+        elif board[px][i] == 0:
+            break
+        elif board[px][i] == a:
+            flag = True
+            for j in range(i,py):
+                board[px][j] = a
+    #Top Right            
+    for i, j in zip(range(px+1,8), range(py+1,8)):
+        if i == px+1 and j == py+1 and board[i][j] != -a:
+            break
+        elif board[i][j] == 0:
+            break
+        elif board[i][j] == a:
+            flag = True
+            for i1, j1 in zip(range(px+1,i), range(py+1,j)):
+                board[i1][j1] = a
+                
+    #Top Left    
+    for i, j in zip(range(px+1,8), range(py-1,-1,-1)):
+        if i == px+1 and j == py-1 and board[i][j] != -a:
+            break
+        elif board[i][j] == 0:
+            break
+        elif board[i][j] == a:
+            flag = True
+            for i1, j1 in zip(range(px+1,i), range(py-1,j,-1)):
+                board[i1][j1] = a
+
+    #Bottom Right
+    for i, j in zip(range(px-1,-1,-1), range(py+1,8)):
+        if i == px-1 and j == py+1 and board[i][j] != -a:
+            break
+        elif board[i][j] == 0:
+            break
+        elif board[i][j] == a:
+            flag = True
+            for i1, j1 in zip(range(px-1,i,-1), range(py+1,j)):
+                board[i1][j1] = a
+    #Bottom Left
+    for i, j in zip(range(px-1,-1,-1), range(py-1,-1,-1)):
+        if i == px-1 and j == py-1 and board[i][j] != -a:
+            break
+        elif board[i][j] == 0:
+            break
+        elif board[i][j] == a:
+            flag = True
+            for i1, j1 in zip(range(px-1,i,-1), range(py-1,j,-1)):
+                board[i1][j1] = a
+
+    return flag
 
 
