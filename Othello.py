b162919a63b4159076e502038264d817d4016da9
ProAltro <90308389+ProAltro@users.noreply.github.com>
2021-10-28 12:58:37 +0530

    Minor changes

--- Othello.py
+++ Othello.py
@@ -10,7 +10,8 @@ plus =  '┼'
 rplus = '├'
 uplus = '┴'
 
-pieces = {-1:"O",0:" ",1:"X"}
+pieces = {-1:"\u25CB",0:" ",1:"\u25CF"}
+
 board = []
 for i in range(8):
     board.append([])
@@ -51,7 +52,7 @@ board[3][3],board[4][4] = -1,-1
 board[3][4],board[4][3] = 1,1
 
 
-def move(p,a):
+def move(p,a,check):
     global board
     px,py = p
     flag = False
@@ -64,8 +65,11 @@ def move(p,a):
             break
         elif board[i][py] == a:
             flag = True
+            if check:
+                break
             for j in range(px,i+1):
                 board[j][py] = a
+            break
 
     #Left        
     for i in range(px-1,-1,-1):
@@ -75,8 +79,12 @@ def move(p,a):
             break
         elif board[i][py] == a:
             flag = True
+            if check:
+                break
             for j in range(i,px+1):
                 board[j][py] = a
+            break
+        
     #Top            
     for i in range(py+1,8):
         if i == py+1 and board[px][i] != -a:
@@ -85,8 +93,11 @@ def move(p,a):
             break
         elif board[px][i] == a:
             flag = True
+            if check:
+                break
             for j in range(py,i+1):
                 board[px][j] = a
+            break
             
         
     #Bottom   
@@ -97,8 +108,12 @@ def move(p,a):
             break
         elif board[px][i] == a:
             flag = True
+            if check:
+                break
             for j in range(i,py+1):
                 board[px][j] = a
+            break
+        
     #Top Right            
     for i, j in zip(range(px+1,8), range(py+1,8)):
         if i == px+1 and j == py+1 and board[i][j] != -a:
@@ -107,8 +122,11 @@ def move(p,a):
             break
         elif board[i][j] == a:
             flag = True
+            if check:
+                break
             for i1, j1 in zip(range(px,i+1), range(py,j+1)):
                 board[i1][j1] = a
+            break
                 
     #Top Left    
     for i, j in zip(range(px+1,8), range(py-1,-1,-1)):
@@ -118,8 +136,11 @@ def move(p,a):
             break
         elif board[i][j] == a:
             flag = True
+            if check:
+                break
             for i1, j1 in zip(range(px,i+1), range(py,j-1,-1)):
                 board[i1][j1] = a
+            break
 
     #Bottom Right
     for i, j in zip(range(px-1,-1,-1), range(py+1,8)):
@@ -129,8 +150,12 @@ def move(p,a):
             break
         elif board[i][j] == a:
             flag = True
+            if check:
+                break
             for i1, j1 in zip(range(px,i-1,-1), range(py,j+1)):
                 board[i1][j1] = a
+            break
+        
     #Bottom Left
     for i, j in zip(range(px-1,-1,-1), range(py-1,-1,-1)):
         if i == px-1 and j == py-1 and board[i][j] != -a:
@@ -139,26 +164,49 @@ def move(p,a):
             break
         elif board[i][j] == a:
             flag = True
+            if check:
+                break
             for i1, j1 in zip(range(px,i-1,-1), range(py,j-1,-1)):
                 board[i1][j1] = a
+            break
 
     return flag
 
+def checkValid(player):
+    for i in range(8):
+        for j in range(8):
+            if move((i,j),player,True):
+                return True
+    return False
+
+
+    
 def Play():
     player = -1
+    print('''
+Rules
+''')
+    Print()
+    print(pieces[player],'\'s turn: ')
     while True:
+        flag = True                    
+        if not checkValid(player):
+            if not checkValid(player*-1):
+                break
+            else:
+                print(pieces[player],"has no valid moves. Turn skipped")
+                player*=-1
+                    
         pos = [int(i) for i in input("Enter location: ").split(',')]
         pos[0],pos[1] = pos[0] - 1, pos[1] - 1
         pos = tuple(pos)
-        while True:
-            if move(pos,player):
-                break
-            else:
-                pos = [int(i) for i in input("Invalid. Enter location: ").split(',')]
-                pos[0],pos[1] = pos[0] - 1, pos[1] - 1
-                pos = tuple(pos)
-        Print()
+        if move(pos,player,False):
+            Print()
+        else:
+            print("Not a Valid move")
+            continue
+            
         player *= -1
-    
-Print()
+        print(pieces[player],'\'s turn: ')
+
 Play()
