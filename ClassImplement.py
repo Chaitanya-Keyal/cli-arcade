009e3523fa0383f782b286d7e79cd56b3ff13bdf
ProAltro <90308389+ProAltro@users.noreply.github.com>
2021-10-31 14:02:39 +0530

    Menu

--- ClassImplement.py
+++ ClassImplement.py
@@ -1403,8 +1403,47 @@ class TicTacToe:
                 else:
                     cho = input("Enter y or n : ").strip().lower()
     
-    
-    
+while True:
+    print("Welcoeme to the Arcade")
+    print('''We offer 3 games:
+1. Tic Tac Toe
+2. MasterMind
+3. Othello''')
+    while True:
+        try:
+            a = input("Enter Game: ")
+            if a == '1' or a.lower() == 'tictactoe' or a.lower() == 'tic tac toe':
+                a = 1
+            elif a == '2' or a.lower == 'mastermind' or a.lower() == 'master mind':
+                a = 2
+            elif a == '3' or a.lower() == 'othello':
+                a = 3
+            else:
+                raise Exception
+            
 
+        except:
+            print("Invalid Option!")
+            continue
 
-a = TicTacToe()
+        if a == 1:
+            b = TicTacToe()
+        elif a == 2:
+            b = MasterMind()
+        elif a == 3:
+            b = Othello()
+
+        ynl = ['y','ye','yes','yep','yup','yeah','yas','yass','yasss','yee',
+       'n','no','nope','na','nah']
+        while True:
+            print("Do you want to play again? (y/n): ")
+            f = input().lower().strip()
+            if f in ynl:
+                break
+            else:
+                print("\nERROR\n\n")      
+        if f in ynl[:10]:
+            continue
+        else:
+            print("Thank you for playing!")
+            break
